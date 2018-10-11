7ba2c6fc94145604e5435b65bc567400e764a932
DESKTOP-79RKK94\MAODOT <872522635@qq.com>
2018-10-11 20:23:17 +0800

    init spring boot 2.0 tomacat web by Spring initializr Page

---
+++ src/main/java/com/maodot/weixinfunction/WeixinfunctionApplication.java
@@ -0,0 +1,15 @@
+package com.maodot.weixinfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MAODOT
+ */
+@SpringBootApplication
+public class WeixinfunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinfunctionApplication.class, args);
+	}
+}
